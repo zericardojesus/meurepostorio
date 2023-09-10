007803cb59a53a9fcc787c0958f9be766b68731c
zericardooliveira <zericardooliveirajesus@gmail.com>
2023-09-10 21:13:53 +0100

    sorteio eletronico

---
+++ arquivo/Math.js
@@ -0,0 +1,15 @@
+/*
+
+math => matematica
+
+-pow(2.2) / potencia / 2 = 4 / power
+-sqrt(25,2) / raiz / v25
+-pi
+-ceil / teto => arredondar pra cima
+-floor / chão =>arredonda para baixo
+-random() / numero aleatorio entre 0 e 1
+
+*/
+
+const result = Math.pow(2,3)
+console.loe(result)

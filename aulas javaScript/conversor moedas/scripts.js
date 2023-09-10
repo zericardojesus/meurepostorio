6dc2209e66ddbe0065a9d0f3c4053e7a1cf5c815
zericardooliveira <zericardooliveirajesus@gmail.com>
2023-09-10 18:56:35 +0100

    mudança no js todo

--- aulas javaScript/conversor moedas/scripts.js
+++ aulas javaScript/conversor moedas/scripts.js
@@ -1,27 +1,60 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currencySelect")
 
-function convertValues(){
+function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToconvert = document.querySelector(".currency-value-to-convert")
-    const currencyValueconverted = document.querySelector(".currency-value")
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueToConverted = document.querySelector(".currency-value")
 
-    const dolarToday = 5.20
-    const euroToday = 5,34
 
-    const convertedValue = inputCurrencyValue / dolarToday
+    const dolarToday = 5.2
+    const euroToday = 6.2
 
-    currencyValueToconvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    if (currencySelect.value == "dolar") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
-    
 
-    currencyValueconverted.innerHTML = new Intl.NumberFormat("es-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
 
-    console.log(convertedValue)
+
 }
 
+function changeCurrency () {
+ const currencyName = document.getElementById("currency-name")
+ const currencyImage = document.querySelector(".currency-img")
+
+if (currencySelect.value == "dolar") {
+    currencyName.innerHTML = "Dólar Americano"
+    currencyImage.src = "./assets/dolar.png"
+}    
+   
+
+if (currencySelect.value == "euro") {
+    currencyName.innerHTML = "Euro"
+    currencyImage.src = "./assets/euro.png"
+
+}   
+
+convertValues()
+
+}
+
+
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
